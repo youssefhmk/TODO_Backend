652eb6f5f6097b395765c05baa2d8415a38e4e24
youssefhmk <youssefhesham2006K@gmail.com>
2026-08-05 23:29:57 +0300

    linking the frontend and backend of the todo list  app

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 import express from "express"
+import path,{dirname} from "path"
+import { fileURLToPath } from "url"
 const app = express()
 const PORT = process.env.PORT || 5000
+const __filename =fileURLToPath(import.meta.url)
+const __dirname =dirname(__filename)
+app.use(express.static(path.join(__dirname,"../public")))
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","index.html"))
+})
 app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`))
